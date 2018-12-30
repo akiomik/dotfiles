1b1b6e4e725f64748d637ec389f4cfcd417b0573
Akiomi Kamakura <akiomik@gmail.com>
2018-12-30 23:26:04 +0900

    changed 4-sp to 2-sp

--- homebrew/brew2brewfile.sh
+++ homebrew/brew2brewfile.sh
@@ -9,19 +9,19 @@ cat /dev/null > $file
 # write taps
 taps=`brew tap`
 for tap in $taps; do
-    echo "tap '$tap'" >> $file
+  echo "tap '$tap'" >> $file
 done
 
 # write leaves
 leaves=`brew leaves`
 for leaf in $leaves; do
-    echo "brew '$leaf'" >> $file
+  echo "brew '$leaf'" >> $file
 done
 
 # write casks
 casks=`brew cask list`
 for cask in $casks; do
-    echo "cask '$cask'" >> $file
+  echo "cask '$cask'" >> $file
 done
 
 echo "$file generated successfully."
