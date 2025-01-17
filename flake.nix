1aecb97120362e53d0b889c689d0e260f46c7d97
Akiomi Kamakura <akiomik@gmail.com>
2025-01-18 01:29:36 +0900

    Remove formater

--- flake.nix
+++ flake.nix
@@ -20,21 +20,7 @@
     ...
   } @ inputs: let
     inherit (self) outputs;
-    # Supported systems for your flake packages, shell, etc.
-    systems = [
-      "aarch64-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    # Formatter for your nix files, available through 'nix fmt'
-    # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#akiomi@trinculo'
     homeConfigurations = {
