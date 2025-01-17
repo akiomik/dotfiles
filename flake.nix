180a9bbbf871f715965e7ad1643f68b85ee39ec0
Akiomi Kamakura <akiomik@gmail.com>
2025-01-17 23:15:42 +0900

    Remove configurations for nixos

--- flake.nix
+++ flake.nix
@@ -41,24 +41,9 @@
 
     # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
-    # Reusable nixos modules you might want to export
-    # These are usually stuff you would upstream into nixpkgs
-    nixosModules = import ./modules/nixos;
     # Reusable home-manager modules you might want to export
     # These are usually stuff you would upstream into home-manager
     homeManagerModules = import ./modules/home-manager;
-
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#trinculo'
-    nixosConfigurations = {
-      trinculo = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          # > Our main nixos configuration file <
-          ./nixos/configuration.nix
-        ];
-      };
-    };
 
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#akiomi@trinculo'
