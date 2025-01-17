be66e06f3d397915dbe5fd4d3e007fedc3625f7a
Akiomi Kamakura <akiomik@gmail.com>
2025-01-17 23:14:13 +0900

    Remove i686-linux support

--- flake.nix
+++ flake.nix
@@ -24,7 +24,6 @@
     # Supported systems for your flake packages, shell, etc.
     systems = [
       "aarch64-linux"
-      "i686-linux"
       "x86_64-linux"
       "aarch64-darwin"
       "x86_64-darwin"
