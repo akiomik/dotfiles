5c3303a7531cef285b14c728f613b9f245dff30f
Akiomi Kamakura <akiomik@gmail.com>
2025-01-17 23:07:59 +0900

    Replace placeholders

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -71,12 +71,11 @@
   # FIXME: Add the rest of your current configuration
 
   # TODO: Set your hostname
-  networking.hostName = "your-hostname";
+  networking.hostName = "trinculo";
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
-    # FIXME: Replace with your username
-    your-username = {
+    akiomi = {
       # TODO: You can set an initial password for your user.
       # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
