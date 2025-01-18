dabca5c25e0a6b9bcb4e5eeb2a38a8d4495433b7
Akiomi Kamakura <akiomik@gmail.com>
2025-01-19 02:58:13 +0900

    Add brew packages

--- nix/home/homebrew.nix
+++ nix/home/homebrew.nix
@@ -3,9 +3,114 @@
 {
   enable = true;
 
-  taps = [];
+  taps = [
+    "auth0/auth0-cli" # auth0
+    "homebrew/test-bot" # brew test-bot
+    "kayac/tap" # ecspresso
+  ];
 
-  brews = [];
+  brews = [
+    # File Format
+    "brotli"
+    "snappy"
+    "webp"
+    "zstd"
+
+    # Editor
+    "emacs" # for agda
+    "neovim"
+
+    # Language
+    "agda"
+    "mise"
+    "rustup"
+
+    # Language server
+    "elixir-ls"
+    "solargraph"
+
+    # Package Manager
+    "cocoapods"
+    "mas"
+    "poetry"
+    "rye"
+    "uv"
+    "yarn"
+
+    # Build Tool
+    "just"
+    "make"
+    "sccache"
+    "wasm-pack"
+
+    # Linter & Formatter
+    "actionlint"
+    "biome"
+    "ruff"
+    "shfmt"
+    "staticcheck"
+    "tflint"
+    "typos-cli"
+
+    # Git
+    "aicommits"
+    "gh"
+    "ghq"
+    "gibo"
+    "git"
+    "git-delta"
+    "git-lfs"
+
+    # AWS
+    "awscli"
+    "copilot"
+    "ecspresso"
+
+    # SaaS
+    "auth0"
+
+    # Container
+    "docker"
+    "docker-completion"
+
+    # Performance
+    "htop"
+    "hyperfine"
+    "k6"
+
+    # Audio & Video
+    "ffmpeg"
+    "flac"
+
+    # Image
+    "imagemagick"
+
+    # Utility
+    "coreutils"
+    "gnu-sed"
+    "gnupg"
+    "jq"
+    "peco"
+    "ripgrep"
+    "tree"
+
+    # Network Tools
+    "curl"
+    "websocat"
+    "wget"
+
+    # Terminal
+    "tmux"
+
+    # Database
+    "postgresql@15"
+    "redis"
+
+    # Shell
+    "starship"
+    "zplug"
+    "zsh-syntax-highlighting"
+  ];
 
   casks = [
     # 📕Books
@@ -21,12 +126,13 @@
     "chromedriver"
     "docker"
     "hex-fiend"
+    "intellij-idea-ce"
     "leapp"
     "ngrok"
     "proxyman"
     "rapidapi"
     "raspberry-pi-imager"
-    "intellij-idea-ce"
+    "session-manager-plugin" # for leapp
     "utm"
     "visual-studio-code"
     "vmware-fusion"
@@ -127,18 +233,21 @@
   ];
 
   # Mac App Store using mas
+  # NOTE: iOS & iPadOS apps are currently not supported
   # https://github.com/mas-cli/mas
   masApps = {
+    ADGUARD_FOR_SAFARI = 1440147259;
+    GARAGEBAND = 682658836;
+    # HAYAEMON = 717639970;
+    IMOVIE = 408981434;
+    KEYNOTE = 409183694;
+    LADIOCAST = 411213048;
     LINE = 539883307;
-
-    # TODO: Ad the followings
-    # はやえもん
-    # Ad guard for safari
-    # save-to-raindrop
-    # ladiocast
-    # "tailscale"
-    # spotify
-    # "nostore"
-    # Wappalyzer
+    NOSTORE = 1666553677;
+    NUMBERS = 409203825;
+    PAGES = 409201541;
+    SAVE_TO_RAINDROP = 1549370672;
+    TAILSCALE = 1475387142;
+    WAPPALYZER = 1520333300;
   };
 }
